7518b22b45e255f7846aeeeec458101547ad045f
Yigit Duman <yigitq@gmail.com>
2026-01-04 18:15:42 +0300

    update default html template to auto resolve ar:// and ipfs:// uris

--- contract/script/UpdateHTMLTemplate.s.sol
+++ contract/script/UpdateHTMLTemplate.s.sol
@@ -5,13 +5,13 @@ import { BaseScript } from "./Base.s.sol";
 import { MURIProtocol } from "../src/MURIProtocol.sol";
 
 contract UpdateHTMLTemplate is BaseScript {
-    function run() public broadcast {
-        // Read minified HTML template
-        string memory htmlTemplate = vm.readFile("html-template/minified.html");
-        MURIProtocol muriProtocol = MURIProtocol(0x42d7C2B7dF3EFfb6B2c9a22F9791b09F72C66d45);
+  function run() public broadcast {
+    // Read minified HTML template
+    string memory htmlTemplate = vm.readFile("html-template/minified.html");
+    MURIProtocol muriProtocol = MURIProtocol(0x0000000000C2A0B63ab4aA971B08B905E5875b01);
 
-        string[] memory templateParts = new string[](1);
-        templateParts[0] = htmlTemplate;
-        muriProtocol.setDefaultHtmlTemplate(templateParts, false);
-    }
+    string[] memory templateParts = new string[](1);
+    templateParts[0] = htmlTemplate;
+    muriProtocol.setDefaultHtmlTemplate(templateParts, false);
+  }
 }
